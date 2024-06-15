ede99859ead982063d7b15f650e5301e2a7c08eb
Ananthuak16 <134612644+Ananthuak16@users.noreply.github.com>
2024-06-15 19:58:36 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,9 @@ import userRoutes from "./routes/userRoute.js";
 import adminRoutes from "./routes/adminRoute.js"; 
 import bookRoutes from "./routes/bookRoute.js"; 
 import borrowingRoutes from "./routes/borrowingRoute.js"; 
-
+import swaggerJsdoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
+import YAML from "yamljs"
 dotenv.config(); // Load environment variables
 
 // Connect to the database
@@ -20,6 +22,16 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser()); // Apply cookie-parser middleware
+
+
+// Parse the Swagger YAML file
+const swaggerDocument = YAML.load("./swagger.yaml");
+
+// Serve Swagger UI
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+
+
 
 // Routes
 app.use('/api/users', userRoutes);
